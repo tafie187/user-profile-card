bc4210c4f4815574e046e2c0d370017fc0176a9e
tafie187 <127634792+tafie187@users.noreply.github.com>
2023-07-26 20:30:20 +0200

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import UserProfileCard from './UserProfileCard';
+import  './App.css'
+import  './UserProfileCard.css'
+
+function App() {
+    return (
+        <div className="Appp">
+            <UserProfileCard />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/UserProfileCard.js
@@ -0,0 +1,38 @@
+import taffie from "./taf.jpg";
+import './App'
+function UserProfileCard() {
+  return (
+    <div className="App">
+    <div className="user-profile-card">
+      <img src={taffie} alt="Taffie Pic" />
+      <h2>Tafadzwa Chijarira</h2>
+      <p>
+        A talented and passionate young developer with a strong ambition to make
+        a mark in the world of technology.
+      </p>
+      <div className="num">
+        <ul className="stats">
+          <li>
+            2000 <br />
+            Followers
+          </li>
+          <li>
+            300 <br />
+            Followers
+          </li>
+          <li>
+            15 <br />
+            Projects
+          </li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="btn">
+        <button>Contact Me</button>
+        <button>View Profile</button>
+      </div>
+    </div>
+    </div>
+  );
+}
+export default UserProfileCard;
