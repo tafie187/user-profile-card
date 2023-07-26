81741e2654ea90e2de8bc11580cba3e0577f497e
tafie187 <127634792+tafie187@users.noreply.github.com>
2023-07-26 21:31:07 +0200

    styling

--- src/UserProfileCard.js
+++ src/UserProfileCard.js
@@ -6,6 +6,7 @@ function UserProfileCard() {
     <div className="user-profile-card">
       <img src={taffie} alt="Taffie Pic" />
       <h2>Tafadzwa Chijarira</h2>
+      <p className="para">Full Stack Developer</p><br/>
       <p>
         A talented and passionate young developer with a strong ambition to make
         a mark in the world of technology.
